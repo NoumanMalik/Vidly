9b81ce3ea76bbf5e62d2e65e531371e248511d5e
Noman Malik <noman.malik@pawnamerica.com>
2024-05-13 11:36:41 +0500

    Data Form

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Xml.Schema;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -22,12 +23,15 @@ namespace Vidly.Controllers
         {
             base.Dispose(disposing);
         }
+        #region Customers List
         // GET: Customer
         public ActionResult Index()
         {
             var getCustomersList = context.Customers.Include(x=>x.MembershipType).ToList();
             return View(getCustomersList);
         }
+        #endregion
+        #region Customer Detail
         public ActionResult Detail(int Id)
         {
             var getCustomer = context.Customers.Include(x=>x.MembershipType).SingleOrDefault(x => x.Id == Id);
@@ -35,6 +39,43 @@ namespace Vidly.Controllers
                 return HttpNotFound();
             return View(getCustomer);
         }
-        
+        #endregion
+        #region Customer Add/Update
+        public ActionResult New()
+        {
+            var membershipType = context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipType = membershipType
+            };
+            return View("Save",viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if(customer.Id == 0)
+                context.Customers.Add(customer);
+            else
+            {
+                var model = context.Customers.Single(x => x.Id == customer.Id);
+                model.Name = customer.Name;
+                model.DateOfBirth = customer.DateOfBirth;
+                model.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+                model.MembershipTypeId = customer.MembershipTypeId;
+            }
+            context.SaveChanges();
+            return RedirectToAction("Index","Customer");
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = context.Customers.Single(x => x.Id == id);
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipType= context.MembershipTypes.ToList()
+            };
+            return View("Save", viewModel);
+        }
+        #endregion
     }
 }
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -22,6 +22,7 @@ namespace Vidly.Models
     {
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Movies> Movies { get; set; }
+        public DbSet<MembershipType> MembershipTypes { get; set; }
         public ApplicationDbContext()
             : base("ConnectionString", throwIfV1Schema: false)
         {
